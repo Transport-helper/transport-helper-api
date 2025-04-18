61029920ea66ede5f5b639edc9e134ce18e0f2d4
PandaX185 <mouss3abbb@gmail.com>
2025-04-18 15:52:05 +0200

    Add initial project structure with Maven wrapper and main application class

---
+++ src/main/java/com/transport/helper_api/HelperApiApplication.java
@@ -0,0 +1,13 @@
+package com.transport.helper_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelperApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelperApiApplication.class, args);
+	}
+
+}
