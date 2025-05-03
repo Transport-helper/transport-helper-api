028f90c64a2cbeed0585d264ec95046a1b782aa4
PandaX185 <mouss3abbb@gmail.com>
2025-05-03 14:42:22 +0300

    Add semantic @Repository annotation

--- src/main/java/com/transport/helper_api/repository/LocationRepository.java
+++ src/main/java/com/transport/helper_api/repository/LocationRepository.java
@@ -2,7 +2,9 @@ package com.transport.helper_api.repository;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 
 import com.transport.helper_api.model.Location;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LocationRepository extends Neo4jRepository<Location, Long> {
 
 }
--- src/main/java/com/transport/helper_api/repository/RouteRepository.java
+++ src/main/java/com/transport/helper_api/repository/RouteRepository.java
@@ -2,8 +2,11 @@ package com.transport.helper_api.repository;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 
 import com.transport.helper_api.model.Route;
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
+@Repository
 public interface RouteRepository extends Neo4jRepository<Route, Long> {
 	
 	Optional<Route> findByStartLocationIdAndEndLocationIdAndModeOfTransport(
