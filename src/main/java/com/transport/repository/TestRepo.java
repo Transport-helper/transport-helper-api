04f3c0d3fe726e58ae6bda5240edeb015819b26d
PandaX185 <mouss3abbb@gmail.com>
2025-05-06 00:02:24 +0300

    Cleanup unneeded classes

--- src/main/java/com/transport/repository/TestRepo.java
+++
@@ -1,9 +0,0 @@
-package com.transport.repository;
-
-import com.transport.model.TestNode;
-import org.springframework.data.neo4j.repository.Neo4jRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TestRepo extends Neo4jRepository<TestNode, Long> {
-}
